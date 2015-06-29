4361f244d5bce3748e408d933fd7792b23b3c47f
Richard Harrah <richard.harrah@anthem.com>
2015-06-29 18:48:37 -0400

    fixing compile issues caused by using GitHub as an IDE

--- src/main/java/com/tealcube/java/maven/plugins/SwaggerCodegenAbstractMojo.java
+++ src/main/java/com/tealcube/java/maven/plugins/SwaggerCodegenAbstractMojo.java
@@ -6,12 +6,12 @@ import org.apache.maven.plugins.annotations.Parameter;
 import java.io.File;
 import java.nio.charset.Charset;
 
-public abstract class SwaggerCodegenAbstractMojo {
+public abstract class SwaggerCodegenAbstractMojo extends AbstractMojo {
 
     protected static final Charset UTF8 = Charset.forName("UTF-8");
     
-    @Parameter(property = SwaggerCodegenPlugin.PREFIX + "sourceDirectory", required = true, defaultValue = "${project.basedir}/src/docs/swagger")
-    protected File sourceDirectory;
+    @Parameter(property = SwaggerCodegenPlugin.PREFIX + "sourceDirectory", required = true)
+    protected String inputSpec;
 
     @Parameter(property = SwaggerCodegenPlugin.PREFIX + "outputDirectory", required = true, defaultValue = "${project.build.directory}/generated")
     protected File outputDirectory;
@@ -19,7 +19,7 @@ public abstract class SwaggerCodegenAbstractMojo {
     @Parameter(property = SwaggerCodegenPlugin.PREFIX + "language", required = true, defaultValue = "java")
     protected String language;
 
-    @Parameter(property = SwaggerCodegen.PREFIX + "skip", required = false)
+    @Parameter(property = SwaggerCodegenPlugin.PREFIX + "skip", required = false)
     protected boolean skip = false;
 
 }
